3826df9bb66d71f08f848ebd602fd054b84b3bed
Gregor Richardson <gregor.com@mac.com>
2017-05-09 20:48:00 -0700

    programatic year creation works, column display doesn't work
    not sure why

--- js/dataTable.js
+++ js/dataTable.js
@@ -1,94 +1,101 @@
 $(document).ready(function() {
 
-	/*
-	$('.data-table').DataTable({
-		    ajax: 'https://gregor.demo.socrata.com/resource/68np-55g5.json'
-	});
-	*/
-	//generateColumns();
-	$('.data-table').DataTable();
-	generateColumns();
+    /*
+    $('.data-table').DataTable({
+    	    ajax: 'https://gregor.demo.socrata.com/resource/68np-55g5.json'
+    });
+    */
+    //generateColumns();
+    $('.data-table').DataTable();
+    generateColumns();
 
-// look to selection menu and determine which table should be rendered
-var metricType = $("#metric").val();
-console.log(metricType);
+    // look to selection menu and determine which table should be rendered
+    var metricType = $("#metric").val();
+    console.log(metricType);
 
-// prepare data endpoint
-var url = 'https://gregor.demo.socrata.com/resource/68np-55g5.json?$limit=10';
-//var url = 'http://data.ny.gov/resource/farmersmarkets.json?$limit=30';
-$.getJSON(url, function(data, textstatus) {
-// console.log(data);
-      // Loop over result set
-      $.each(data, function(i, column) {
-console.log(data[i]);
-      //if (data[i]["_2010"] !== undefined && data[i]["_2011"]!== undefined) {
-      	if (data[i]["_2010"] == undefined) {
-      		data[i]["_2010"] = '-';
-      	}
-      	if (data[i]["_2011"] == undefined) {
-      		data[i]["_2011"] = '-';
-      	}
+    // prepare data endpoint
+    var url = 'https://gregor.demo.socrata.com/resource/68np-55g5.json?$limit=10';
 
-      	 // for each json record, add a row to the HTML table
-         $('.data-table').dataTable().fnAddData( [
-	       column.life_expectancy,
-	       column._2010,
-	       column._2011
-	       ]);
 
-     // }; //end if
+    $.getJSON(url, function(data, textstatus) {
+        // console.log(data);
 
 
 
-      }); // end $.each
-}); // end $.getJSON
+        // Loop over result set
+        $.each(data, function(i, column) {
+            //console.log(data[i]);
+            for (var x = 2005; x < 2012; x++) {
 
-/*
-var url = 'http://data.ny.gov/resource/farmersmarkets.json?$limit=30';
-$.getJSON(url, function(data, textstatus) {
-      // Loop over result set
-      $.each(data, function(i, column) {
-         // for each json record, add a row to the HTML table
-         $('.data-table').dataTable().fnAddData( [
-	       column.contact ,
-	       column.city,
-	       column.county ]
-         );
-      }); // end $.each
-}); // end $.getJSON
-*/
+                // if current year is blank, set the text to "-"
+                if (data[i]["_" + x] == undefined) {
+                    data[i]["_" + x] = '-';
+                    console.log(data[i]["_" + x]);
+                };
+            }; // end for x
 
-//generate data table columns
-function generateColumns() {
-	var table = $(".data-table thead tr");
-	for (var i = 1950; i < 2012; i++) {
-		$(table).append("<th>_"+i+"</th>");
-		// console.log("I'm adding a header");
-	};
-}
+            // for each json record, add a row to the HTML table
+            $('.data-table').dataTable().fnAddData([
+                column.life_expectancy,
+                column._2008,
+                column._2009,
+                column._2010,
+                column._2011
+            ]);
 
 
-// look for menu selection change. Trigger table function
-$("#metric").on("change", function(){
-	metricType = $("#metric").val();
-	fillTable(metricType);
-});
+        }); // end $.each
 
 
-// table function
-function fillTable(metrictype) {
-	// select generic table by class. change id of the table to match drop down, then populate table with ajax
-	if (metrictype == 'Two') {
-		$(".data-table").attr("id","tableTwo");
-		console.log("some ajax call goes here for metric Two");
-	} else if (metrictype == 'Three') {
-		$(".data-table").attr("id","tableThree");
-		console.log("some ajax call goes here for metric Three");
-	} else {
-		console.log("I don't know what you just set that to");
-	}
 
-}
+    }); // end $.getJSON
+
+    /*
+    var url = 'http://data.ny.gov/resource/farmersmarkets.json?$limit=30';
+    $.getJSON(url, function(data, textstatus) {
+          // Loop over result set
+          $.each(data, function(i, column) {
+             // for each json record, add a row to the HTML table
+             $('.data-table').dataTable().fnAddData( [
+    	       column.contact ,
+    	       column.city,
+    	       column.county ]
+             );
+          }); // end $.each
+    }); // end $.getJSON
+    */
+
+    //generate data table columns
+    function generateColumns() {
+        var table = $(".data-table thead tr");
+        for (var i = 1950; i < 2012; i++) {
+            $(table).append("<th>_" + i + "</th>");
+            // console.log("I'm adding a header");
+        };
+    }
+
+
+    // look for menu selection change. Trigger table function
+    $("#metric").on("change", function() {
+        metricType = $("#metric").val();
+        fillTable(metricType);
+    });
+
+
+    // table function
+    function fillTable(metrictype) {
+        // select generic table by class. change id of the table to match drop down, then populate table with ajax
+        if (metrictype == 'Two') {
+            $(".data-table").attr("id", "tableTwo");
+            console.log("some ajax call goes here for metric Two");
+        } else if (metrictype == 'Three') {
+            $(".data-table").attr("id", "tableThree");
+            console.log("some ajax call goes here for metric Three");
+        } else {
+            console.log("I don't know what you just set that to");
+        }
+
+    }
 
 
 
